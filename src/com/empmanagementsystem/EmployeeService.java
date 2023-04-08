3c7598689e1f4a231134f8206066eca3a3983722
SharforazRahman <111514061+SharforazRahman@users.noreply.github.com>
2023-04-08 17:06:58 +0600

    Crud Operation

--- src/com/empmanagementsystem/EmployeeService.java
+++ src/com/empmanagementsystem/EmployeeService.java
@@ -6,12 +6,13 @@ import java.util.Scanner;
 public class EmployeeService {
     HashSet<Employee> empSet = new HashSet<Employee>();
 
-    Employee employee1 = new Employee(101,"sharforaz",23,"CSE","Developer",30000);
-    Employee employee2 = new Employee(102,"Tahmid",24,"CSE","Developer",40000);
-    Employee employee3 = new Employee(103,"Polock",23,"CSE","Developer",35000);
-    Employee employee4 = new Employee(104,"Turjo",23,"CSE","Developer",30000);
+    Employee employee1 = new Employee(101, "sharforaz", 23, "CSE", "Developer", 30000);
+    Employee employee2 = new Employee(102, "Tahmid", 24, "CSE", "Developer", 40000);
+    Employee employee3 = new Employee(103, "Polock", 23, "CSE", "Developer", 35000);
+    Employee employee4 = new Employee(104, "Turjo", 23, "CSE", "Developer", 30000);
 
     Scanner sc = new Scanner(System.in);
+    boolean found = false;
     int id;
     String name;
     int age;
@@ -24,5 +25,88 @@ public class EmployeeService {
         empSet.add(employee2);
         empSet.add(employee3);
         empSet.add(employee4);
+    }
+
+    //View All Employee
+    public void viewAllEmp() {
+        for (Employee emp : empSet) System.out.println(emp);
+    }
+
+    //View Employee based on their Id;
+    public void viewEmp() {
+        System.out.println("Enter ID:");
+        id = sc.nextInt();
+        for (Employee employee : empSet) {
+            if (employee.getId() == id) {
+                found = true;
+                System.out.println(employee);
+            }
+        }
+        if(!found){
+            System.out.println("Employee is not found");
+        }
+    }
+
+    //Update Employee;
+
+    public void updateEmployee(){
+        System.out.println("Enter ID");
+        id = sc.nextInt();
+        boolean found = false;
+        for(Employee emp : empSet){
+            if(emp.getId() == id){
+                System.out.println("Enter Name");
+                name = sc.next();
+                System.out.println("Enter New Salary");
+                sal = sc.nextDouble();
+                emp.setName(name);
+                emp.setSalary(sal);
+                System.out.println("Updated Details of Employee are: ");
+                System.out.println(emp);
+                found = true;
+            }
+        }
+        if(!found) System.out.println("Employee not found!");
+        else System.out.println("Employee updated successfully");
+    }
+
+    //Delete Employee;
+    public void deleteEmp(){
+        System.out.println("Enter id");
+        id = sc.nextInt();
+        boolean found = false;
+        Employee empDelete = null;
+        for(Employee i : empSet){
+            if(i.getId() == id){
+                empDelete = i;
+                found = true;
+            }
+        }
+        if(!found) System.out.println("Employee is not present");
+        else{
+            empSet.remove(empDelete);
+            System.out.println("Employee Deleted Successfully");
+        }
+    }
+
+    //Add Employee
+    public void addEmp(){
+        System.out.println("Enter ID:");
+        id = sc.nextInt();
+        System.out.println("Enter Name");
+        name = sc.next();
+        System.out.println("Enter age");
+        age = sc.nextInt();
+        System.out.println("Enter Designation");
+        designation = sc.next();
+        System.out.println("Enter Department");
+        department = sc.next();
+        System.out.println("Enter Salary");
+        sal = sc.nextDouble();
+
+        Employee employee = new Employee(id,name,age,designation,department,sal);
+        empSet.add(employee);
+        System.out.println(employee);
+        System.out.println("Employee Added Successfully");
     }
 }
--- src/com/empmanagementsystem/Main.java
+++ src/com/empmanagementsystem/Main.java
@@ -7,41 +7,51 @@ public class Main {
     static boolean ordering = true;
 
     public static void menu() {
-        System.out.println("Welcome to Employee Management System");
+        System.out.println("                           Welcome to Employee Management System");
+        System.out.println("                    -----------------------------------------------------");
+        System.out.println();
         System.out.println("""
-                1.Add Student
-                2.Show All Students
-                3.Get Student based on roll Number
-                4.Delete Student
-                5.Update Student
+                1.Add Employee
+                2.View Employee
+                3.Update Employee
+                4.Delete Employee
+                5.View all Employee
                 6.Exist""");
     }
 
     public static void main(String[] args) {
+        EmployeeService service = new EmployeeService();
         Scanner sc = new Scanner(System.in);
         do {
             menu();
             System.out.println("Enter your Choice please: ");
             int choice = sc.nextInt();
             switch (choice) {
-                case 1:
+                case 1 -> {
                     System.out.println("Add Employee");
-                    break;
-                case 2:
+                    service.addEmp();
+                }
+                case 2 -> {
                     System.out.println("View Employee");
-                    break;
-                case 3:
+                    service.viewEmp();
+                }
+                case 3 -> {
                     System.out.println("Update Employee");
-                    break;
-                case 4:
+                    service.updateEmployee();
+                }
+                case 4 -> {
                     System.out.println("Delete Employee");
-                    break;
-                case 5:
+                    service.deleteEmp();
+                }
+                case 5 -> {
+                    System.out.println("View All Employee");
+                    service.viewAllEmp();
+                }
+                case 6 -> {
                     System.out.println("Thank you For Using Application !!");
                     System.exit(0);
-                default:
-                    System.out.println("Please Enter Valid Choice");
-                    break;
+                }
+                default -> System.out.println("Please Enter Valid Choice");
             }
         } while (ordering);
     }
